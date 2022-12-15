1f4767a03dade97ba8d73e0582e24153ed3a10c6
Lilya <lilia.lilya.s@mail.ru>
2022-12-15 16:04:28 +0300

    homework-2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
+import { useEffect, useState } from 'react'
 import { Message } from './components/Message'
+import { Form } from './components/Form/Form'
+import { MessageList } from './components/Message/MessageList.'
+
 
 export function App() {
+
+    const [message, setMessage] = useState([]);
+    const addMess = (newMess) => {
+        setMessage([...message, newMess]);
+    }
+
+    useEffect(() => {
+        if (message.length > 0 && message[message.length - 1].author === 'user') {
+            const timeout = setTimeout(() => {
+                addMess({
+                    author: 'bot',
+                    text: 'bot it`s me'
+                })
+            }, 1500);
+
+            return () => {
+                clearTimeout(timeout);
+            }
+        }
+
+
+
+    }, [message])
+
     return (
         <>
             <Message title='Переданный пропсом текст' />
+            <Form addMessage={addMess} />
+            <MessageList mess={message}></MessageList>
         </>
     )
 }
